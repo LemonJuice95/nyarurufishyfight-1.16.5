c5e36ad8423dcaa213be446e83308cd3b9732972
Administrator <1582017385@qq.com>
2022-05-04 12:26:16 +0800

    1

--- src/main/java/com/nyaruru/loot/NChestLootTables.java
+++ src/main/java/com/nyaruru/loot/NChestLootTables.java
@@ -13,15 +13,15 @@ public class NChestLootTables implements Consumer<BiConsumer<ResourceLocation, L
     @Override
     public void accept(BiConsumer<ResourceLocation, LootTable.Builder> t) {
         t.accept(NLoots.DARK_FOREST_LOOT_CHEST, LootTable.lootTable()
-                .withPool(LootPool.lootPool().setRolls(RandomValueRange.between(3.0F, 5.0F))
-                        .add(ItemLootEntry.lootTableItem(NItemRegister.CABBAGE.get()).setWeight(16))
-                        .add(ItemLootEntry.lootTableItem(NItemRegister.CANNED_CAT_FOOD.get()).setWeight(16))
-                        .add(ItemLootEntry.lootTableItem(NItemRegister.COPPER_COIN.get()).setWeight(20)).apply(SetCount.setCount(RandomValueRange.between(1.0F, 2.0F)))
-                        .add(ItemLootEntry.lootTableItem(NItemRegister.SILVER_COIN.get()).setWeight(18))
-                        .add(ItemLootEntry.lootTableItem(NItemRegister.GOLD_COIN.get()).setWeight(14))
-                        .add(ItemLootEntry.lootTableItem(NItemRegister.CAT_DIAMOND.get()).setWeight(12))
-                        .add(ItemLootEntry.lootTableItem(NItemRegister.THORN_DAGGER.get()).setWeight(12))
-                        .add(ItemLootEntry.lootTableItem(NItemRegister.EMERGENCY_POTION.get()).setWeight(12))
-                        .add(EmptyLootEntry.emptyItem().setWeight(5))));
+                .withPool(LootPool.lootPool().setRolls(RandomValueRange.between(4.0F, 8.0F))
+                        .add(ItemLootEntry.lootTableItem(NItemRegister.CABBAGE.get()).setWeight(6))
+                        .add(ItemLootEntry.lootTableItem(NItemRegister.CANNED_CAT_FOOD.get()).setWeight(6))
+                        .add(ItemLootEntry.lootTableItem(NItemRegister.COPPER_COIN.get()).setWeight(10)).apply(SetCount.setCount(RandomValueRange.between(1.0F, 2.0F)))
+                        .add(ItemLootEntry.lootTableItem(NItemRegister.SILVER_COIN.get()).setWeight(8))
+                        .add(ItemLootEntry.lootTableItem(NItemRegister.GOLD_COIN.get()).setWeight(4))
+                        .add(ItemLootEntry.lootTableItem(NItemRegister.CAT_DIAMOND.get()).setWeight(2))
+                        .add(ItemLootEntry.lootTableItem(NItemRegister.THORN_DAGGER.get()).setWeight(2))
+                        .add(ItemLootEntry.lootTableItem(NItemRegister.EMERGENCY_POTION.get()).setWeight(2))
+                        .add(EmptyLootEntry.emptyItem().setWeight(6))));
     }
 }
