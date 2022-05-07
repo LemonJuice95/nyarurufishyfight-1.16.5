da12f1d80aa58c4b91c9d092b12fb4b2d5f7413a
Administrator <1582017385@qq.com>
2022-05-08 06:15:24 +0800

    1

--- src/main/java/com/nyaruru/blocks/misc/CrystalClusterBlock.java
+++ src/main/java/com/nyaruru/blocks/misc/CrystalClusterBlock.java
@@ -12,13 +12,17 @@ import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.stats.Stats;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.world.NoteBlockEvent;
 
 import javax.annotation.Nullable;
@@ -28,7 +32,20 @@ public class CrystalClusterBlock extends Block {
     public static final VoxelShape SHAPE = Block.box(4.0F, 0.0F, 4.0F, 12.0F, 15.0F, 12.0F);
 
     public CrystalClusterBlock() {
-        super(AbstractBlock.Properties.of(Material.DECORATION).strength(0.2F).sound(SoundType.STONE).isSuffocating(NBlockRegister::never).isViewBlocking(NBlockRegister::never));
+        super(AbstractBlock.Properties.of(Material.DECORATION).strength(0.2F).sound(SoundType.STONE).isSuffocating(NBlockRegister::never).isViewBlocking(NBlockRegister::never)
+                .lightLevel((state) -> {
+                    return 7;
+                }));
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public void animateTick(BlockState p_180655_1_, World p_180655_2_, BlockPos p_180655_3_, Random p_180655_4_) {
+        double d0 = (double)p_180655_3_.getX() + 0.55D - (double)(p_180655_4_.nextFloat() * 0.1F);
+        double d1 = (double)p_180655_3_.getY() + 0.55D - (double)(p_180655_4_.nextFloat() * 0.1F);
+        double d2 = (double)p_180655_3_.getZ() + 0.55D - (double)(p_180655_4_.nextFloat() * 0.1F);
+        if (p_180655_4_.nextInt(5) == 0) {
+            p_180655_2_.addParticle(ParticleTypes.END_ROD, d0, d1, d2, p_180655_4_.nextGaussian() * 0.005D, p_180655_4_.nextGaussian() * 0.005D, p_180655_4_.nextGaussian() * 0.005D);
+        }
     }
 
     @SuppressWarnings("deprecation")
@@ -54,4 +71,6 @@ public class CrystalClusterBlock extends Block {
         }
         super.playerDestroy(p_180657_1_, p_180657_2_, p_180657_3_, p_180657_4_, p_180657_5_, p_180657_6_);
     }
+
+
 }
