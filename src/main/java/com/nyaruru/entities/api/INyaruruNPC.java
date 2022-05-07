02722f42b0545589875b1f05ed0c0cadff1fd5ec
Administrator <1582017385@qq.com>
2022-05-07 22:14:54 +0800

    1

--- src/main/java/com/nyaruru/entities/api/INyaruruNPC.java
+++ src/main/java/com/nyaruru/entities/api/INyaruruNPC.java
@@ -2,6 +2,6 @@ package com.nyaruru.entities.api;
 
 import com.nyaruru.entities.NPCTypes;
 
-public interface INyaruruNPC {
+public interface INyaruruNPC extends INyaruruEntity{
     public abstract NPCTypes getNPCType();
 }
