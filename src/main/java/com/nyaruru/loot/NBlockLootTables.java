7366635d59121f9131bc3df0ba83ff0971a92683
Administrator <1582017385@qq.com>
2022-05-08 04:44:35 +0800

    1

--- src/main/java/com/nyaruru/loot/NBlockLootTables.java
+++ src/main/java/com/nyaruru/loot/NBlockLootTables.java
@@ -1,6 +1,7 @@
 package com.nyaruru.loot;
 
 import com.nyaruru.blocks.NBlockRegister;
+import com.nyaruru.items.NItemRegister;
 import net.minecraft.advancements.criterion.EnchantmentPredicate;
 import net.minecraft.advancements.criterion.ItemPredicate;
 import net.minecraft.advancements.criterion.MinMaxBounds;
@@ -8,12 +9,10 @@ import net.minecraft.block.Block;
 import net.minecraft.data.loot.BlockLootTables;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.item.Items;
-import net.minecraft.loot.ConstantRange;
-import net.minecraft.loot.ItemLootEntry;
-import net.minecraft.loot.LootPool;
-import net.minecraft.loot.LootTable;
+import net.minecraft.loot.*;
 import net.minecraft.loot.conditions.ILootCondition;
 import net.minecraft.loot.conditions.MatchTool;
+import net.minecraft.loot.functions.SetCount;
 import net.minecraft.util.ResourceLocation;
 
 import java.util.function.BiConsumer;
@@ -33,6 +32,7 @@ public class NBlockLootTables extends BlockLootTables {
 
         this.add(NBlockRegister.THORNS.get(), NBlockLootTables::createThornsDrop);
         this.add(NBlockRegister.CRYSTAL_CLUSTER.get(), NBlockLootTables::createCrystalClusterDrop);
+        this.add(NBlockRegister.COIN_PILE.get(), NBlockLootTables::createCoinPileDrop);
     }
 
     private static LootTable.Builder createThornsDrop(Block block) {
@@ -41,9 +41,17 @@ public class NBlockLootTables extends BlockLootTables {
                     .add(ItemLootEntry.lootTableItem(block)));
     }
 
-    private  static LootTable.Builder createCrystalClusterDrop(Block block) {
+    private static LootTable.Builder createCrystalClusterDrop(Block block) {
         return LootTable.lootTable().withPool(LootPool.lootPool()
                 .when(HAS_SILK_TOUCH).setRolls(ConstantRange.exactly(1))
                     .add(ItemLootEntry.lootTableItem(block)));
+    }
+
+    private static LootTable.Builder createCoinPileDrop(Block block) {
+        return LootTable.lootTable().withPool(LootPool.lootPool()
+                .setRolls(RandomValueRange.between(2.0F, 4.0F))
+                .add(ItemLootEntry.lootTableItem(NItemRegister.COPPER_COIN.get()).apply(SetCount.setCount(RandomValueRange.between(1.0F, 3.0F))))
+                .add(ItemLootEntry.lootTableItem(NItemRegister.SILVER_COIN.get()).apply(SetCount.setCount(RandomValueRange.between(1.0F, 3.0F))))
+                .add(ItemLootEntry.lootTableItem(NItemRegister.GOLD_COIN.get()).apply(SetCount.setCount(RandomValueRange.between(1.0F, 2.0F)))));
     }
 }
